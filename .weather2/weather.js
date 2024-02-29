8ed6c0f1cb1610fd3e97d1928b481e2045890343
Brad Snyder <bsnyder13@gmail.com>
2024-02-28 21:52:02 -0500

    started JS

--- .weather2/weather.js
+++ .weather2/weather.js
@@ -1,1 +1,13 @@
-export function getWeather(lat, long, timezone)
+import axios from "axios"
+
+// https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime
+
+export function getWeather(lat, lon, timezone) {
+    return axios.get("https://api.open-meteo.com/v1/forecast?&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime", { params: {
+        lattitude: lat,
+        longitude: lon,
+        timezone,
+        },
+    }
+) 
+}   
